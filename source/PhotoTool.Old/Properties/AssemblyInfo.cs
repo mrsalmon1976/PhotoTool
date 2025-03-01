7e1356718b6696e66bbf33b5d5a0627dc5a3d162
Matt Salmon <mrsalmon@gmail.com>
2025-03-01 05:29:44 +0200

    Moved v3 into ".Old" folders and namespace

--- source/PhotoTool.Old/Properties/AssemblyInfo.cs
+++ source/PhotoTool.Old/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.1.0")]
 [assembly: AssemblyFileVersion("3.1.0")]
-[assembly: InternalsVisibleTo("PhotoTool.Test")]
+[assembly: InternalsVisibleTo("PhotoTool.Old.Test")]
