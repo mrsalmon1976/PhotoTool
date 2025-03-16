e814430d652adb4ed15c9051a5acaa57455a41cb
Matt Salmon <mrsalmon@gmail.com>
2025-03-16 11:40:11 +0200

    Added unit tests for ImageViewModel

--- source/PhotoTool.Tests/Features/BatchResizer/ViewModels/ImageViewModelTests.cs
+++ source/PhotoTool.Tests/Features/BatchResizer/ViewModels/ImageViewModelTests.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Avalonia;
+using Avalonia.Headless.NUnit;
+using Avalonia.Media.Imaging;
+using NUnit.Framework;
+using PhotoTool.Features.BatchResizer.ViewModels;
+using PhotoTool.Tests.Random;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +18,31 @@ namespace PhotoTool.Tests.Features.BatchResizer.ViewModels
         [Test]
         public void Dimensons_WhenImageNull_ReturnsEmptyString()
         {
-            Assert.Fail();
+            // Arrange
+            ImageViewModel imageView = new ImageViewModel();
+
+            // Act
+            string result = imageView.Dimensions;   
+
+            // Assert
+            Assert.That(result, Is.EqualTo(string.Empty));
         }
 
-        [Test]
+        [AvaloniaTest]
         public void Dimensons_WhenImageNotNull_ReturnsValidString()
         {
-            Assert.Fail();
+            // Arrange
+            int width = RandomData.Number.Next(800, 1024);
+            int height = RandomData.Number.Next(600, 768);
+            using Bitmap image = new Bitmap(TestAssets.PhotoToolLogoPath);
+            using Bitmap testImage = image.CreateScaledBitmap(new PixelSize(width, height));
+
+            ImageViewModel imageView = new ImageViewModel();
+            imageView.Image = testImage;
+
+            // Act
+            Assert.That(imageView.Dimensions, Is.EqualTo($"{width} x {height}"));
+
         }
 
     }
