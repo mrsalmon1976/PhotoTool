511bf4f10f09c4fe28517d5b67db9f764984e845
Matt Salmon <mrsalmon@gmail.com>
2025-03-16 10:30:01 +0200

    Added unit tests for ImageResizeOptionsValidator

--- source/PhotoTool.Tests/Features/BatchResizer/Validators/ImageResizeOptionsValidatorTests.cs
+++ source/PhotoTool.Tests/Features/BatchResizer/Validators/ImageResizeOptionsValidatorTests.cs
@@ -1,4 +1,8 @@
 ﻿using NUnit.Framework;
+using PhotoTool.Features.BatchResizer.Models;
+using PhotoTool.Features.BatchResizer.Validators;
+using PhotoTool.Shared.Exceptions;
+using PhotoTool.Test;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,25 +17,79 @@ namespace PhotoTool.Tests.Features.BatchResizer.Validators
         [Test]
         public void Validate_ValidModel_NoExceptionThrown()
         {
-            Assert.Fail();
+            // setup
+            ImageResizeOptions options = new SubstituteBuilder<ImageResizeOptions>()
+                .WithRandomProperties()
+                .WithProperty(x => x.MaxImageLength, (uint)1024)
+                .WithProperty(x => x.MaxThumbnailLength, (uint)100)
+                .Build();
+
+            // execute
+            IImageResizeOptionsValidator validator = new ImageResizeOptionsValidator();
+            validator.Validate(options);
+
+            // assert
+            Assert.Pass();
         }
 
         [Test]
         public void Validate_InvalidMaxImageLength_ExceptionThrown()
         {
-            Assert.Fail();
+            // setup
+            ImageResizeOptions options = new SubstituteBuilder<ImageResizeOptions>()
+                .WithRandomProperties()
+                .WithProperty(x => x.MaxImageLength, (uint)0)
+                .WithProperty(x => x.MaxThumbnailLength, (uint)100)
+                .Build();
+
+            // execute
+            IImageResizeOptionsValidator validator = new ImageResizeOptionsValidator();
+            var exception = Assert.Throws<ValidationException>(() => validator.Validate(options));
+
+            // assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception?.Errors.Count, Is.EqualTo(1));
+            Assert.That(exception?.Errors[0], Does.Contain("Image length"));
         }
 
         [Test]
         public void Validate_InvalidMaxThumbnailLength_ExceptionThrown()
         {
-            Assert.Fail();
+            // setup
+            ImageResizeOptions options = new SubstituteBuilder<ImageResizeOptions>()
+                .WithRandomProperties()
+                .WithProperty(x => x.MaxImageLength, (uint)1024)
+                .WithProperty(x => x.GenerateThumbnails, true)
+                .WithProperty(x => x.MaxThumbnailLength, (uint)0)
+                .Build();
+
+            // execute
+            IImageResizeOptionsValidator validator = new ImageResizeOptionsValidator();
+            var exception = Assert.Throws<ValidationException>(() => validator.Validate(options));
+
+            // assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception?.Errors.Count, Is.EqualTo(1));
+            Assert.That(exception?.Errors[0], Does.Contain("Thumbnail length"));
         }
 
         [Test]
         public void Validate_InvalidMaxThumbnailLengthButGenerateThumbnailsFalse_NoExceptionThrown()
         {
-            Assert.Fail();
+            // setup
+            ImageResizeOptions options = new SubstituteBuilder<ImageResizeOptions>()
+                .WithRandomProperties()
+                .WithProperty(x => x.MaxImageLength, (uint)1024)
+                .WithProperty(x => x.GenerateThumbnails, false)
+                .WithProperty(x => x.MaxThumbnailLength, (uint)0)
+                .Build();
+
+            // execute
+            IImageResizeOptionsValidator validator = new ImageResizeOptionsValidator();
+            validator.Validate(options);
+
+            // assert
+            Assert.Pass();
         }
 
     }
